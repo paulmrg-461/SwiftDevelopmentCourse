69f523a6fa4e3cefbbd4b450c4ac652adfe4da97
Paul Realpe <paulmrg461@gmail.com>
2020-12-02 12:13:19 -0500

    Initial Commit

---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Paul Realpe on 2/12/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
