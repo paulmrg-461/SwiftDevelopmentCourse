30de346efc4b94d8389615fa13b30d7eca59c0fb
Paul Realpe <paulmrg461@gmail.com>
2020-12-04 07:37:04 -0500

    2. Assignment and arithmetic operators

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,10 +7,27 @@
 
 import SwiftUI
 
+let a = 5
+var b = 10
+
+
+// Tuplas
+let (x,y) = (1,2)
+
+//D/d -> D == d*c*r
+//9/4 //c
+//9%4 //r
+//9 == 4*2+1 //true
+
+
+let name = "Paul Realpe"
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        if name == "Paul Realpe" {
+        Text("Hola \(name), el resultado de la suma de 5 + 10 es = \(a+b)")
             .padding()
+        }
     }
 }
 
