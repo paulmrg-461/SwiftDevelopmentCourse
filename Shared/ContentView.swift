16b7be914c450b669c687548e2c39a183687738a
Paul Realpe <paulmrg461@gmail.com>
2020-12-04 13:19:48 -0500

    6. Ranks

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -31,6 +31,9 @@ var userAge: Int?
 var ageToBeUsed = userAge ?? defaultAge
 //ageToBeUsed = (userAge != nil ? userAge! : defaultAge)
 
+//Rangos
+let names = ["Paul", "Jimmy","Diana"]
+
 struct ContentView: View {
     var body: some View {
         Text("Hello World").padding()
@@ -55,4 +58,32 @@ func getRowHeight() -> Void {
 
 func getRowHeightTernaryOperator() -> Void {
     rowHeight = contentHeight + (hasImage ? 100 : 10)
+}
+
+//Ciclo for rango cerrado 1-5
+func getRankOneToFive() -> Void {
+    for idx in 1...5 {
+        print(idx)
+    }
+}
+
+//Ciclo for rango semiabierto
+func getSemiopenRankOneToFour() -> Void {
+    for idx in 1..<5{
+        print(idx)
+    }
+}
+
+//Ciclo for rango abierto en array
+func getNamesInArray() -> Void {
+    for i in 0..<names.count{
+        print("La persona \(i+1) se llama \(names[i])")
+    }
+}
+
+//Ciclo for rangos dinamicos
+func getDinamycRankInArray() -> Void {
+    for name in names[1...]{
+        print(name)
+    }
 }
