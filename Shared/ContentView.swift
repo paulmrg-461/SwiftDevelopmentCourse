2896d6999aca3066c0f477167fa31d9999f5292d
Paul Realpe <paulmrg461@gmail.com>
2020-12-10 12:26:05 -0500

    28. Switch in compound cases

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -19,48 +19,14 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-func switchInterval() -> Void {
-    let moons = 90
-    let naturalCount: String
-    let phrase = "Lunas en Saturno"
-
-    switch moons {
-        case 0:
-            naturalCount = "No hay"
-        case 1..<5:
-            naturalCount = "Hay unas pocas"
-        case 5..<12:
-            naturalCount = "Hay bastantes"
-        case 12..<100:
-            naturalCount = "Hay decenas de"
-        case 100..<1000:
-            naturalCount = "Hay centenares de"
-        default:
-            naturalCount = "Hay muchisisisisisisiiimas"
-    }
+func switchCompound() -> Void {
+    let anotherPoint = (0,9)
     
-    print("\(naturalCount) \(phrase)")
-}
-
-func switchTuplas() -> Void {
-    let somePoint = (1,1)
-
-    switch somePoint {
-        case (0,0):
-            print("El punto \(somePoint) es el origen de coordenadas.")
-        case (_,0):
-            print("El punto \(somePoint) se encuentra sobre el eje X.")
-        case (0,_):
-            print("El punto \(somePoint) se encuentra sobre el eje Y.")
-        case (-2...2, -2...2):
-            print("El punto \(somePoint) se encuentra en el interior del cuadrado de lado 4.")
-        //Recuperar el valor de los elementos de las tuplas
-        case (let x, 0):
-                print("Sobre el eje X, con valor \(x)")
-        case let (x,y) where x==y:
-            print("El punto se encuentra sobre la recta x = y")
-        default:
-            print("El punto \(somePoint) no se encuentra en las coordenadas")
+    switch anotherPoint {
+    case (let distance, 0), (0, let distance):
+        print("Se encuentra sobre el eje, a distancia \(distance) del origen.")
+    default:
+        print("No se encuentra sobre el eje")
     }
 }
 
